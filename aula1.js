91f4e8e7a9d24030fabe1fbbc010eec6928edd1b
jessicabraga18 <jsilvaaaa@gmail.com>
2023-05-24 10:59:38 -0300

    incluindo Dom

---
+++ aula1.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1')
+titulo.textContent = "Aula"
+
+let titulo2 = document.querySelector("#primeiro")
+titulo2.textContent = 'caixa'
+
+let testando = document.querySelector('#paragrafo')
+testando.textContent = "Escrever correto "
+
